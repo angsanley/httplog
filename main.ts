65a8cbd9bbd5a98e7c0c0243bb4793d1f3b2fce8
Stanley <angstanley68@gmail.com>
2025-08-27 10:39:13 +0700

    initial commit

---
+++ main.ts
@@ -0,0 +1,24 @@
+import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+
+const app = new Hono()
+
+// Use app.notFound to handle wildcard routes
+export const customLogger = (message: string, ...rest: unknown[]) => {
+  console.log(message, ...rest)
+}
+
+app.use(logger(customLogger))
+
+app.notFound((c) => {
+  const headers = c.req.header()
+  const body = c.req.raw.body
+
+  customLogger("Headers:", headers || '<empty>')
+  customLogger("Body:", body || '<empty>')
+
+  return c.json({ message: 'Ok' }, 200)
+})
+
+
+Deno.serve(app.fetch)
